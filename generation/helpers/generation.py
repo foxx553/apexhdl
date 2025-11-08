c3b88cf8e95512f7f0485e859841385394bacb6d
foxx553 <delhonf@gmail.com>
2025-11-08 11:23:26 -0500

    Fix generation, with unary one-hot encoding

--- generation/helpers/generation.py
+++ generation/helpers/generation.py
@@ -188,47 +188,43 @@ def unary_method(args):
     # Args extraction
     evaluator_name = args[0]
     function_of_x = args[1]
-    data_width = args[2]
+    data_width = int(args[2])
     x_min, x_max, y_min, y_max = map(float, args[3:7])
 
     # Discrete output calculation
     y_discrete_values = compute_discrete_output(function_of_x, data_width, x_min, x_max, data_width, y_min, y_max)
 
     # Computing unary core routing logic
-    unary_core_array = [[0 for _ in range(2**data_width)] for _ in range(2**data_width)]
-    y_cursor = 0
-    unary_core_array[0][0] = 1
-    for x_value, y_value in enumerate(y_discrete_values):
-        if y_value != y_cursor:
-            step = 1 if y_value > y_cursor else -1
-            for i in range(y_cursor + step, y_value + step, step):
-                unary_core_array[i][x_value] = 1
-            y_cursor = y_value
-
+    unary_core_array = [[-1] for _ in range(2**data_width)]
+    for output_value in range(2**data_width):
+        for x_value, y_value in enumerate(y_discrete_values):
+            if y_value == output_value:
+                if unary_core_array[output_value][0] == -1:
+                    unary_core_array[output_value][0] = x_value
+                else:
+                    unary_core_array[output_value].append(x_value)
+    
     # Converting routing logic to VHDL code
     unary_core_code = ""
-    for y_value, x_values in enumerate(unary_core_array):
-        has_one = False
-        for x_value, is_connected in enumerate(x_values):
-            if is_connected == 1:
-                if not has_one:
-                    unary_core_code += f"\tdecoder_input({y_value}) <= encoder_output({x_value})"
-                    has_one = True
+    for output_value in range(len(unary_core_array)):
+        if unary_core_array[output_value][0] != -1:
+            for index, x_value in enumerate(unary_core_array[output_value]):
+                if index == 0:
+                    unary_core_code += f"\tdecoder_input({output_value}) <= encoder_output({x_value})"
                 else:
-                    unary_core_code += f" xor encoder_output({x_value})"
-        if has_one:
-            unary_core_code += f";\n"
+                    unary_core_code += f" or encoder_output({x_value})"
+            unary_core_code += ";\n"
 
     # VHDL complete code generation
     return f"""
 -------------------------------------
--- Engineers: Florian Delhon, Kevin Peymani
+-- Engineer: Florian Delhon
 -- Target: PYNQ-Z2
 -- Module Name: {evaluator_name}
 -- Function: f(x) = {function_of_x}
 -- Evaluator method: Unary
 -- Data width: {data_width} bits
--- Range: x in [{x_min}; {x_max}[, y in [{y_min}; {y_max}[
+-- Range: x in [{x_min};{x_max}[, y in [{y_min};{y_max}[
 -------------------------------------
 
 library IEEE;
@@ -252,37 +248,26 @@ architecture arch_{evaluator_name} of {evaluator_name} is
 
 begin
 
-    -- Thermometer encoder
-    thermometer_encoder: process(input_a)
-        variable input_int : integer;
+    -- One-hot encoder    
+    one_hot_encoder: process(input_a)
     begin
-        input_int := to_integer(unsigned(input_a));
-        for i in 0 to 2**DATA_WIDTH - 1 loop
-            if i <= input_int then
-                encoder_output(i) <= '1';
-            else
-                encoder_output(i) <= '0';
-            end if;
-        end loop;
-    end process thermometer_encoder;
+        encoder_output <= (others => '0');
+        encoder_output(to_integer(unsigned(input_a))) <= '1';
+    end process one_hot_encoder;
 
-
-    -- Unary core
+    -- Unary code
 {unary_core_code}
 
-    -- Thermometer decoder
-    thermometer_decoder: process(decoder_input)
-        variable count : integer := 0;
+    -- One-hot decoder
+    one_hot_decoder: process(decoder_input)
     begin
-        count := 0;
-        for i in 1 to 2**DATA_WIDTH - 1 loop
+        result <= (others => '0');
+        for i in 0 to decoder_input'length - 1 loop
             if decoder_input(i) = '1' then
-                count := count + 1;
+                result <= std_logic_vector(to_unsigned(i, result'length));
             end if;
         end loop;
-        result <= std_logic_vector(to_unsigned(count, DATA_WIDTH));
-    end process thermometer_decoder;
-
+    end process one_hot_decoder;
 
 end arch_{evaluator_name};
     """
@@ -317,31 +302,27 @@ def hybrid_method(args):
     core_output_width = math.ceil(math.log2(largest_core_value))
     
     # Computing unary core routing logic
-    unary_core_array = [[[0 for _ in range(2**data_width)] for _ in range(2**data_width)] for _ in range(2**segment_idx_width)]
+    unary_core_array = [[[-1] for _ in range(2**core_output_width)] for _ in range(2**segment_idx_width)]
     for segment_idx, discrete_values in enumerate(subfunctions):
-        y_cursor = 0
-        unary_core_array[segment_idx][0][0] = 1
-        for x_value, y_value in enumerate(discrete_values):
-            if y_value != y_cursor:
-                step = 1 if y_value > y_cursor else -1
-                for i in range(y_cursor + step, y_value + step, step):
-                    unary_core_array[segment_idx][i][x_value] = 1
-                y_cursor = y_value
-    
+        for output_value in range(2**core_output_width):
+            for x_value, y_value in enumerate(discrete_values):
+                if y_value == output_value:
+                    if unary_core_array[segment_idx][output_value][0] == -1:
+                        unary_core_array[segment_idx][output_value][0] = x_value
+                    else:
+                        unary_core_array[segment_idx][output_value].append(x_value)
+
     # Converting routing logic to VHDL code
     unary_core_code = ""
     for segment_idx, core_array in enumerate(unary_core_array):
-        for y_value, x_values in enumerate(core_array):
-            has_one = False
-            for x_value, is_connected in enumerate(x_values):
-                if is_connected == 1:
-                    if not has_one:
-                        unary_core_code += f"\tcore_{segment_idx}_output({y_value}) <= encoder_output({x_value})"
-                        has_one = True
+        for output_value in range(len(core_array)):
+            if core_array[output_value][0] != -1:
+                for index, x_value in enumerate(core_array[output_value]):
+                    if index == 0:
+                        unary_core_code += f"\tcore_{segment_idx}_output({output_value}) <= encoder_output({x_value})"
                     else:
-                        unary_core_code += f" xor encoder_output({x_value})"
-            if has_one:
-                unary_core_code += f";\n"
+                        unary_core_code += f" or encoder_output({x_value})"
+                unary_core_code += ";\n"
         unary_core_code += "\n"
 
     # Declaring all core signals
@@ -413,20 +394,12 @@ begin
     least_significants <= input_a(DATA_WIDTH - SEGMENT_IDX_WIDTH - 1 downto 0);
     bias <= BIAS_TABLE(to_integer(unsigned(selector)));
 
-    -- Thermometer encoder
-    thermometer_encoder: process(least_significants)
-        variable input_int : integer;
+    -- One-hot encoder    
+    one_hot_encoder: process(least_significants)
     begin
-        input_int := to_integer(unsigned(least_significants));
-        for i in 0 to 2**(DATA_WIDTH - SEGMENT_IDX_WIDTH) - 1 loop
-            if i <= input_int then
-                encoder_output(i) <= '1';
-            else
-                encoder_output(i) <= '0';
-            end if;
-        end loop;
-    end process thermometer_encoder;
-
+        encoder_output <= (others => '0');
+        encoder_output(to_integer(unsigned(least_significants))) <= '1';
+    end process one_hot_encoder;
 
     -- Unary cores
 {unary_core_code}
@@ -434,19 +407,17 @@ begin
     -- Mux
 {mux_code}
 
-    -- Thermometer decoder
-    thermometer_decoder: process(core_value)
-        variable count : integer := 0;
+    -- One-hot decoder
+    one_hot_decoder: process(core_value)
     begin
-        count := 0;
-        for i in 1 to LARGEST_CORE_VALUE loop
+        subfunction_value <= (others => '0');
+        for i in 0 to core_value'length - 1 loop
             if core_value(i) = '1' then
-                count := count + 1;
+                subfunction_value <= std_logic_vector(to_unsigned(i, subfunction_value'length));
             end if;
         end loop;
-        subfunction_value <= std_logic_vector(to_unsigned(count, DATA_WIDTH));
-    end process thermometer_decoder;
-
+    end process one_hot_decoder;
+    
     adder: process(subfunction_value, bias)
         variable sum : integer := 0;
     begin
